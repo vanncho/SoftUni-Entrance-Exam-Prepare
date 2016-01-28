1ede537aab17c381a7b95eb953a2effcd5f7c3ba
vanncho <van_cho@abv.bg>
2016-01-28 06:49:57 +0000

    Started to prepare for the entrance еxam in SoftUni for 24.04.2016

---
+++ 001.Cake Tycoon/CakeTycoon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class CakeTycoon
+{
+    static void Main()
+    {
+        ulong n = ulong.Parse(Console.ReadLine()); // amount of cakes Ivancho wants.
+        decimal c = decimal.Parse(Console.ReadLine()); // kilograms of flour needed to make one cake.
+        long f = uint.Parse(Console.ReadLine()); // kilograms of flour available.
+        long t = uint.Parse(Console.ReadLine()); // amount of truffles available.
+        long p = uint.Parse(Console.ReadLine()); // amount of truffles available.
+
+        // Cakes he can make
+        decimal cakesHeCanMake = (f / c); // 4621.371477495885
+
+        long trufflesCost = t * p;
+        decimal ddd = (decimal)trufflesCost / n;
+        decimal cakePrice = 1.25m * ddd;
+        decimal TotalFlourRequired = n * c;
+        decimal kilogramsFlourNeeded = TotalFlourRequired - f;
+
+        if (cakesHeCanMake < n)
+        {
+            Console.WriteLine("Can make only {0} cakes, need {1:0.00} kg more flour", (int)cakesHeCanMake, kilogramsFlourNeeded);
+        }
+        if (cakesHeCanMake >= n)
+        {
+            Console.WriteLine("All products available, price of a cake: {0:0.00}", cakePrice);
+        }
+        
+    }
+}
---
+++ 002.Beer Stock/BeerStock.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+class BeerStock
+{
+    static void Main()
+    {
+        long reservedBeers = long.Parse(Console.ReadLine());
+        List<long> beersVariety = new List<long>();
+        List<string> beersPacks = new List<string>();
+
+        while (true)
+        {
+            string[] line = Console.ReadLine().Split(new char [] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (line[0] == "Exam")
+            {
+                break;
+            }
+            beersVariety.Add(long.Parse(line[0]));
+            beersPacks.Add(line[1]);
+        }
+
+        long sumOfBeers = SumOfBeers(beersVariety, beersPacks);
+        long sumOfBeersNeeded = 0;
+
+        long beersNeeded = 0;
+        long sixpacksNeeded = 0;
+        long casesNeeded = 0;
+        long beersAvailable = 0;
+
+        if (sumOfBeers > 99)
+        {
+            sumOfBeers = sumOfBeers - (sumOfBeers / 100);
+        }
+
+        if (sumOfBeers >= reservedBeers)
+        {
+            beersAvailable = sumOfBeers - reservedBeers;
+
+            beersNeeded = beersAvailable % 24;
+            casesNeeded = beersAvailable / 24;
+
+            if (beersNeeded >= 6)
+            {
+                sixpacksNeeded = beersNeeded / 6;
+                beersNeeded = beersNeeded % 6;
+            }
+
+            Console.WriteLine("Cheers! Beer left: {0} cases, {1} sixpacks and {2} beers.", casesNeeded, sixpacksNeeded, beersNeeded);
+        }
+        else
+        {
+            beersAvailable = sumOfBeers - reservedBeers;
+
+            if (beersAvailable < 0)
+            {
+                beersAvailable *= -1;
+            }
+
+            beersNeeded = beersAvailable % 24;
+            casesNeeded = beersAvailable / 24;
+
+            if (beersNeeded >= 6)
+            {
+                sixpacksNeeded = beersNeeded / 6;
+                beersNeeded = beersNeeded % 6;
+            }
+
+            Console.WriteLine("Not enough beer. Beer needed: {0} cases, {1} sixpacks and {2} beers.", casesNeeded, sixpacksNeeded, beersNeeded);
+        }
+
+    }
+
+    private static long SumOfBeers(List<long> beersVariety, List<string> beersPacks)
+    {
+        long sumOfBeers = 0;
+
+        for (int i = 0; i < beersVariety.Count; i++)
+        {
+            if (beersPacks[i] == "sixpacks")
+            {
+                beersVariety[i] = beersVariety[i] * 6;
+            }
+            if (beersPacks[i] == "cases")
+            {
+                beersVariety[i] = beersVariety[i] * 24;
+            }
+
+            sumOfBeers += beersVariety[i];
+        }
+        return sumOfBeers;
+    }
+}
+
