043d165a3a75d08c40238cdb407e596ded450a9d
vanncho <van_cho@abv.bg>
2016-02-04 14:20:53 +0000

    C# Basics Exam 12 April 2014 Morning (All problems solved)

--- 020.Bit Roller/BitRoller.cs
+++ 020.Bit Roller/BitRoller.cs
@@ -10,12 +10,10 @@ class BitRoller
         int r = int.Parse(Console.ReadLine());
 
         string nInBinary = Convert.ToString(n, 2).PadLeft(19, '0');
-        //char[] array = nInBinary.ToCharArray();
-
         StringBuilder bin = new StringBuilder(nInBinary);
         char charAtFPos = bin[bin.Length - 1 - f];
         bin.Remove(bin.Length - 1 - f, 1);
-        // 0000000100111011001
+
         for (int i = 0; i < r; i++)
         {
             char right = bin[bin.Length - 1];
